210c8395e1054df1f62b17a36dbba190ee86b73d
brianb <brianb@cascadia.edu>
2018-02-28 19:42:11 -0800

    Add project files.

---
+++ many-to-many-entities/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace many_to_many_entities
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
